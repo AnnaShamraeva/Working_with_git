6dd0a07b8f1b3b423b26b3cba8b6f6fe94bfe6bf
AnnaShamraeva <anna_plenkina@mail.ru>
2025-10-30 18:04:15 +0300

    Update calculator.py add print

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def sum(a,b):
+        return a+b
+    
+    def extract(a,b):
+        return sum(a,-b)
+    
+    def multiply(a,b):
+        return a*b
+    
+    def divide(a,b):
+        return a/b 
+    
+    def print():
+        print("I am calculator")
