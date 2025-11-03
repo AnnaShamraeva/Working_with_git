d735cc17c1c351e436be9fac544a68c9bfb63db7
Anna Shamraeva <anna_plenkina@mail.ru>
2025-11-03 09:51:21 +0300

    New tests

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 From webinar
 
 This is a repository
+
+Как подтянуть изменения с GitHub 
+PS C:\my_first_vscode_project\Working_with_git> git branch --set-upstream-to=origin/devel devel
+branch 'devel' set up to track 'origin/devel'.
+PS C:\my_first_vscode_project\Working_with_git> git pull
+
+32 Pytest
--- calculator.py
+++ calculator.py
@@ -1,15 +1,19 @@
-class Calculator:
-    def sum(a,b):
+class Calculator: 
+
+    def sum(self, a, b): # self указывает на то, что это методы класса
         return a+b
-    
-    def extract(a,b):
-        return sum(a,-b)
-    
-    def multiply(a,b):
-        return a*b
-    
-    def divide(a,b):
-        return a/b 
-    
-    def print():
-        print("I am calculator")
+ 
+    def extract(self, a, b):
+        # пример: разность a и b
+        return self.sum(a, - b)
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("division by zero")
+        return a / b
+
+    def __str__(self):
+        return 'Hi, I am a calculator'
